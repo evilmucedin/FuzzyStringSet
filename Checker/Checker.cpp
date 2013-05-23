8a2421f47faff18d277a2557ccb56d401a94ffc4
Den Raskovalov <denbox@gmail.com>
2013-05-23 00:14:58 -0700

    checker boilerplate

---
+++ Checker/Checker.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+#include "..\Common\FileIO.h"
+
+class THashCheker
+{
+public:
+	THashCheker(const std::string& filename)
+	{
+		
+	}
+};
+
+int main()
+{
+	TLineReader fInput("..\\surls");
+	char* buffer;
+	size_t urlLen;
+	while (fInput)
+	return 0;
+}
